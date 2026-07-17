af853594285f100d2ad2903ed5e84685a33da3fc
YM <yurii.mocherniuk@protonmail.com>
2026-07-17 14:51:25 +0300

    Update perfect-sentence-shadowing-windows.lua

--- perfect-sentence-shadowing-windows.lua
+++ perfect-sentence-shadowing-windows.lua
@@ -89,8 +89,9 @@ end
 
 local function overlay()
     if not (exists(orig) and exists(rec)) then return osd("Record first") end
+    stop_players()
     ffmpeg({"-i", orig, "-i", rec, "-filter_complex",
-            "[1:a]silenceremove=start_periods=1:start_threshold=-35dB,volume=3dB[r];" ..
+            "[1:a]volume=3dB[r];" ..
             "[0:a][r]amix=inputs=2:duration=longest", mix},
         function(_, res)
             if res and res.status == 0 then play(mix) else osd("Overlay failed") end
